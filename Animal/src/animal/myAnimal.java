430f3a982d4017e1950255e1a713203206030ce5
Jeriel Gaa <jerielgaa142@gmail.com>
2024-02-21 23:22:25 +0800

    Update myAnimal.java

--- Animal/src/animal/myAnimal.java
+++ Animal/src/animal/myAnimal.java
@@ -4,10 +4,10 @@ public class myAnimal {
 
     public static void main(String[] args) {
     	
-        Animal myPet = new Animal("Fluffy", 5, "spayed dog", 8, "Female", 12);
-        myPet.printValues();
+        Pet dog = new Pet("Fluffy", 5, "spayed dog", 8, "Female", 12);
+        dog.printValues();
         
-        Pet pet = new Pet ("Eidref", 50, "Elephant", 24000, "Male", 15);
-        pet.printValues();
+        Animal elephant = new Animal ("Eidref", 50, "Elephant", 24000, "Male", 15);
+        elephant.printValues();
     }
 }
