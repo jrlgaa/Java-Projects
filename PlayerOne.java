294c2661550ee6b69c79b8644ac4c7ad096782a0
Jeriel Gaa <jerielgaa142@gmail.com>
2024-03-03 09:37:06 +0800

    Add files via upload

---
+++ PlayerOne.java
@@ -0,0 +1,24 @@
+package versus;
+
+public class PlayerOne {
+			     
+			    public void playerOneDetails() {
+        System.out.println("Player 1 Weapons: \n1. Sword \n2. Dagger \n3. Long Sword");
+    }
+			    public class playerOneDmg {
+        int Sword, Dagger, LongSword, totalDamage;
+			    			
+        public playerOneDmg() {
+            Sword = (int)(Math.random() * 100) + 1;
+            Dagger = (int)(Math.random() * 100) + 1;
+            LongSword = (int)(Math.random() * 100) + 1; 
+  	        			totalDamage = Sword + Dagger + LongSword;
+            System.out.println("\nPlayer 1 Weapon Damage:");
+            System.out.println("Sword Dmg:      " + Sword);
+            System.out.println("Dagger Dmg:     " + Dagger);
+            System.out.println("Long Sword Dmg: " + LongSword);
+            System.out.println("Total Damage:   " + totalDamage);
+        }
+    }
+			
+}
